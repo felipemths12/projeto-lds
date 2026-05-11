b963b7f58e25bccc6c0471e3d3051548115fff66
felipemths12 <felipe.matheussms@gmail.com>
2026-05-11 20:48:29 -0300

    add: front-end and back-end structures

---
+++ backend/src/main/java/com/projetolds/projetolds/ProjetoldsApplication.java
@@ -0,0 +1,13 @@
+package com.projetolds.projetolds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoldsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoldsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetolds/projetolds/ProjetoldsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetolds.projetolds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoldsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
