e71a53a58d52ac5dfa9f33d0004b0cff7df1e277
felipemths12 <felipe.matheussms@gmail.com>
2026-05-26 20:54:21 -0300

    add: Funcionario Service adicionado

--- backend/src/main/java/com/projetolds/projetolds/dto/funcionario/FuncionarioAtualizacaoDTO.java
+++ backend/src/main/java/com/projetolds/projetolds/dto/funcionario/FuncionarioAtualizacaoDTO.java
@@ -2,6 +2,5 @@ package com.projetolds.projetolds.dto.funcionario;
 
 public record FuncionarioAtualizacaoDTO(Long id_funcionario,
                                         String nome,
-                                        String cpf,
                                         String perfil_cargo) {
 }
--- backend/src/main/java/com/projetolds/projetolds/service/FuncionarioService.java
+++ backend/src/main/java/com/projetolds/projetolds/service/FuncionarioService.java
@@ -1,5 +1,6 @@
 package com.projetolds.projetolds.service;
 
+import com.projetolds.projetolds.dto.funcionario.FuncionarioAtualizacaoDTO;
 import com.projetolds.projetolds.dto.funcionario.FuncionarioCadastroDTO;
 import com.projetolds.projetolds.dto.funcionario.FuncionarioListagemDTO;
 import com.projetolds.projetolds.model.Funcionario;
@@ -8,6 +9,7 @@ import com.projetolds.projetolds.model.enums.StatusGeral;
 import com.projetolds.projetolds.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -31,5 +33,29 @@ public class FuncionarioService {
 
     public List<FuncionarioListagemDTO> listarTodosFuncionarios() {
         return funcionarioRepository.findAll().stream().map(FuncionarioListagemDTO::new).toList();
+    }
+
+    @Transactional
+    public Funcionario atualizarFuncionario(FuncionarioAtualizacaoDTO funcionarioAtualizacaoDTO) {
+        Funcionario funcionario = funcionarioRepository.findById(funcionarioAtualizacaoDTO.id_funcionario())
+                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado."));
+
+        if (funcionarioAtualizacaoDTO.nome() != null) {
+            funcionario.setNome(funcionarioAtualizacaoDTO.nome());
+        }
+
+        if (funcionarioAtualizacaoDTO.perfil_cargo() != null) {
+            funcionario.setPerfil_cargo(Cargo.valueOf(funcionarioAtualizacaoDTO.perfil_cargo()));
+        }
+
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @Transactional
+    public void inativarFuncionario(Long id) {
+        Funcionario funcionario = funcionarioRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Funcioário não encontrado"));
+
+        funcionario.setStatus_ativo(StatusGeral.INATIVO);
     }
 }
