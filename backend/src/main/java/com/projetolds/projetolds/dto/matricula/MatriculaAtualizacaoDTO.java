088217447b4a629b7803f668468192288d3135d2
felipemths12 <felipe.matheussms@gmail.com>
2026-05-27 16:19:20 -0300

    add: DTO de atualizção de Matrícula

---
+++ backend/src/main/java/com/projetolds/projetolds/dto/matricula/MatriculaAtualizacaoDTO.java
@@ -0,0 +1,6 @@
+package com.projetolds.projetolds.dto.matricula;
+
+public record MatriculaAtualizacaoDTO(Long numero_matricula,
+                                      Long codigo_turma,
+                                      String status_matricula) {
+}
