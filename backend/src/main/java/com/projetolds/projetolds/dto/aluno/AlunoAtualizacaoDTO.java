1a9b394a412ebafb008abcbdc9d93646e6c0fda2
felipemths12 <felipe.matheussms@gmail.com>
2026-05-27 15:18:24 -0300

    add: DTO de atualização para Aluno

---
+++ backend/src/main/java/com/projetolds/projetolds/dto/aluno/AlunoAtualizacaoDTO.java
@@ -0,0 +1,7 @@
+package com.projetolds.projetolds.dto.aluno;
+
+public record AlunoAtualizacaoDTO(Long codigo_aluno,
+                                  String nome,
+                                  String email,
+                                  String senha) {
+}
