1644ae7e08e32532a416faf9ad3b9c79cddf5c3c
felipemths12 <felipe.matheussms@gmail.com>
2026-05-28 17:13:20 -0300

    add: serviço de autenticação para processar o login (buscar usuário pelo nome de usuário - email)

--- backend/src/main/java/com/projetolds/projetolds/security/service/AutenticacaoService.java
+++ backend/src/main/java/com/projetolds/projetolds/security/service/AutenticacaoService.java
@@ -1,4 +1,19 @@
 package com.projetolds.projetolds.security.service;
 
-public class AutenticacaoService {
+import com.projetolds.projetolds.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String nomeDeUsuario) {
+        return alunoRepository.findByEmail(nomeDeUsuario);
+    }
 }
