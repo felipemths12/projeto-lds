9736f716badbba0abeb6341b48089e98f128faa7
felipemths12 <felipe.matheussms@gmail.com>
2026-05-28 16:57:56 -0300

    feat: atualizando Repository para que o usuário aluno seja buscado pelo email no banco de dados

--- backend/src/main/java/com/projetolds/projetolds/repository/AlunoRepository.java
+++ backend/src/main/java/com/projetolds/projetolds/repository/AlunoRepository.java
@@ -2,6 +2,8 @@ package com.projetolds.projetolds.repository;
 
 import com.projetolds.projetolds.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface AlunoRepository extends JpaRepository <Aluno, Long> {
+    UserDetails findByEmail(String email);
 }
