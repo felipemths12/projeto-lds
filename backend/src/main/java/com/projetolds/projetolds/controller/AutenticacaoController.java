f6b26f0e2a0e4b7ae3f7700b04ee3e9fccf2d24f
felipemths12 <felipe.matheussms@gmail.com>
2026-05-28 17:31:41 -0300

    add: controller responsável por efetuar o login

---
+++ backend/src/main/java/com/projetolds/projetolds/controller/AutenticacaoController.java
@@ -0,0 +1,35 @@
+package com.projetolds.projetolds.controller;
+
+import com.projetolds.projetolds.dto.login.LoginDTO;
+import com.projetolds.projetolds.dto.login.TokenDTO;
+import com.projetolds.projetolds.model.Aluno;
+import com.projetolds.projetolds.security.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginDTO loginDTO) {
+        var tokenAutenticacao = new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.senha());
+
+        var autenticacao = manager.authenticate(tokenAutenticacao);
+
+        Aluno alunoLogado = (Aluno) autenticacao.getPrincipal();
+        String tokenJWT = tokenService.geradorDeToken(alunoLogado);
+
+        return ResponseEntity.ok(new TokenDTO(tokenJWT));
+    }
+}
