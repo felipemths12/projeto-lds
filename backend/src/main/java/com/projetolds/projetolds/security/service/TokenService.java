63da11b41fe5e8bbe6b181625cc294fb0be8563f
felipemths12 <felipe.matheussms@gmail.com>
2026-05-28 17:20:14 -0300

    add: lógica inicial para geração de token

---
+++ backend/src/main/java/com/projetolds/projetolds/security/service/TokenService.java
@@ -0,0 +1,24 @@
+package com.projetolds.projetolds.security.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.projetolds.projetolds.model.Aluno;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private String senhaSecreta = "12345678";
+
+    public String geradorDeToken(Aluno aluno) {
+        Algorithm algorithm = Algorithm.HMAC256(senhaSecreta);
+        return JWT.create()
+                .withIssuer("Projeto LDS")
+                .withSubject(aluno.getEmail())
+                .withExpiresAt(LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00")))
+                .sign(algorithm);
+    }
+}
