48a1c30982e458348f9ecafcda8fe8ce4e842251
felipemths12 <felipe.matheussms@gmail.com>
2026-05-29 20:45:29 -0300

    add: função para que o Spring busque o CPF como nome de usuário

--- backend/src/main/java/com/projetolds/projetolds/repository/FuncionarioRepository.java
+++ backend/src/main/java/com/projetolds/projetolds/repository/FuncionarioRepository.java
@@ -2,6 +2,8 @@ package com.projetolds.projetolds.repository;
 
 import com.projetolds.projetolds.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+    UserDetails findByCPF(String cpf);
 }
