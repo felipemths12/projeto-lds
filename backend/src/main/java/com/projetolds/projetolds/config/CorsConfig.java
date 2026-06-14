0c6330ac9fb7532df8dccc7541b3b3a835a1f591
felipemths12 <felipe.matheussms@gmail.com>
2026-06-14 14:05:45 -0300

    fix: adicionando url do projeto em deploy

--- backend/src/main/java/com/projetolds/projetolds/config/CorsConfig.java
+++ backend/src/main/java/com/projetolds/projetolds/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                    .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173", "https://sistema-gestao-escolar-2b8tko088-felipemaia-5229s-projects.vercel.app/")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*")
                     .allowCredentials(true);
